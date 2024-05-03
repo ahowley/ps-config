1164d85f5047c0d0fa323b9c8125ba90b36791ee
Alex Howley <howley.alex@gmail.com>
2024-05-02 22:00:28 -0500

    clear terminal on startup

--- setup.ps1
+++ setup.ps1
@@ -4,5 +4,6 @@ Set-Alias -Name oh-my-posh "$env:USERPROFILE\AppData\Local\Programs\oh-my-posh\b
 Add-Content -Path "$PSHOME\Profile.ps1" -Value "[Console]::OutputEncoding = [Text.Encoding]::UTF8"
 Add-Content -Path "$PSHOME\Profile.ps1" -Value "Set-Alias -Name oh-my-posh `"`$env:USERPROFILE\AppData\Local\Programs\oh-my-posh\bin\oh-my-posh.exe`""
 Add-Content -Path "$PSHOME\Profile.ps1" -Value "oh-my-posh init pwsh --config `"https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/gruvbox.omp.json`" | Invoke-Expression"
+Add-Content -Path "$PSHOME\Profile.ps1" -Value "clear"
 oh-my-posh font install --user meslo
 . $PSHOME\Profile.ps1
