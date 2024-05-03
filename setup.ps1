f747995cc35a54030bbbaab37a3757f6f3609d0e
Alex Howley <howley.alex@gmail.com>
2024-05-02 21:57:36 -0500

    initial

---
+++ setup.ps1
@@ -0,0 +1,8 @@
+[Console]::OutputEncoding = [Text.Encoding]::UTF8
+winget install JanDeDobbeleer.OhMyPosh -s winget
+Set-Alias -Name oh-my-posh "$env:USERPROFILE\AppData\Local\Programs\oh-my-posh\bin\oh-my-posh.exe"
+Add-Content -Path "$PSHOME\Profile.ps1" -Value "[Console]::OutputEncoding = [Text.Encoding]::UTF8"
+Add-Content -Path "$PSHOME\Profile.ps1" -Value "Set-Alias -Name oh-my-posh `"`$env:USERPROFILE\AppData\Local\Programs\oh-my-posh\bin\oh-my-posh.exe`""
+Add-Content -Path "$PSHOME\Profile.ps1" -Value "oh-my-posh init pwsh --config `"https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/gruvbox.omp.json`" | Invoke-Expression"
+oh-my-posh font install --user meslo
+. $PSHOME\Profile.ps1
