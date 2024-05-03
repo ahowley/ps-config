5f5ff4489d607ee08b91ba40f2b307238252f108
Alex Howley <howley.alex@gmail.com>
2024-05-02 22:42:45 -0500

    add nvim alias

--- setup.ps1
+++ setup.ps1
@@ -2,6 +2,7 @@
 winget install JanDeDobbeleer.OhMyPosh -s winget
 Set-Alias -Name oh-my-posh "$env:USERPROFILE\AppData\Local\Programs\oh-my-posh\bin\oh-my-posh.exe"
 Set-Alias -Name lazygit "$env:USERPROFILE\AppData\Local\Microsoft\WinGet\Packages\JesseDuffield.lazygit_Microsoft.Winget.Source_8wekyb3d8bbwe\lazygit.exe"
+Set-Alias -Name vim "nvim"
 Add-Content -Path "$PSHOME\Profile.ps1" -Value "[Console]::OutputEncoding = [Text.Encoding]::UTF8"
 Add-Content -Path "$PSHOME\Profile.ps1" -Value "Set-Alias -Name oh-my-posh `"`$env:USERPROFILE\AppData\Local\Programs\oh-my-posh\bin\oh-my-posh.exe`""
 Add-Content -Path "$PSHOME\Profile.ps1" -Value "Set-Alias -Name lazygit `"`$env:USERPROFILE\AppData\Local\Microsoft\WinGet\Packages\JesseDuffield.lazygit_Microsoft.Winget.Source_8wekyb3d8bbwe\lazygit.exe`""
